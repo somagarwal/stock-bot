83e11dc9f0f77e19ddb79940016411da9562b322
somagarwal <agarwal.som@gmail.com>
2026-05-16 22:17:01 +0530

    Update update_sheet.py

--- update_sheet.py
+++ update_sheet.py
@@ -16,7 +16,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
 # अपनी गूगल शीट की ID यहाँ डालें (URL के बीच का हिस्सा)
-spreadsheet_id = "यहाँ_अपनी_शीट_की_ID_डालें" 
+spreadsheet_id = "1terTD1YXV1e26_UCTU74_o6C55ZKdox1jfzanvWpTdM" 
 worksheet = client.open_by_key(spreadsheet_id).worksheet("Top 250 Stocks")
 
 # 2. NSE UDiFF Data Fetcher
