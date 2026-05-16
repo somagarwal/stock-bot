88b864cf246fd50b497a68dd792db32c348f96cc
somagarwal <agarwal.som@gmail.com>
2026-05-16 21:48:31 +0530

    Update stock-bot.py

--- stock-bot.py
+++ stock-bot.py
@@ -9,7 +9,7 @@ import os
 import json
 
 # 1. Credentials Setup
-creds_json = os.environ.get('STOCK_BOT')
+creds_json = os.environ.get('GCP_CREDENTIALS')
 creds_dict = json.loads(creds_json)
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
